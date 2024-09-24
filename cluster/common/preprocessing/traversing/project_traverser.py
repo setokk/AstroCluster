055d6db464a38f15923c39d57ad2b8bf2a813ac2
setokk <73780295+setokk@users.noreply.github.com>
2024-09-24 16:34:16 +0300

    Refactored project_traverser.py

--- cluster/common/preprocessing/traversing/project_traverser.py
+++ cluster/common/preprocessing/traversing/project_traverser.py
@@ -13,19 +13,19 @@ def get_project_files(traverser_strategy: TraverserStrategy,
     # Files to be returned
     files = []
     file_paths = []
-    
+
     # Create regex for valid extensions
     valid_extensions = r'\.(' + '|'.join(file_extensions) + r')$'
     for file_path in glob.iglob(project_path + '/**', recursive=True):
-        # Exclude unwanted extensions and 'module-info.java'
-        if (traverser_strategy.check_file_name(file_path, valid_extensions)):
-            try:
-                with open(file_path, 'r') as f:
-                    file_content = f.read()
-                    if (traverser_strategy.check_file_content(file_content)):
-                        files.append(file_content)
-                        file_paths.append(file_path)
-            except Exception as e:
-                print(f'Error reading file {file_path}: {e}')
-    
+        if (not traverser_strategy.check_file_name(file_path, valid_extensions)):
+            continue
+        try:
+            with open(file_path, 'r') as f:
+                file_content = f.read()
+                if (traverser_strategy.check_file_content(file_content)):
+                    files.append(file_content)
+                    file_paths.append(file_path)
+        except Exception as e:
+            print(f'Error reading file {file_path}: {e}')
+
     return files, file_paths
