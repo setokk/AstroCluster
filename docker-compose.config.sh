6c8ae2b1a7b8261974005191cfa0343e9288c52d
setokk <tesmtracks@gmail.com>
2024-11-05 13:39:27 +0200

    Improved build script

--- docker-compose.config.sh
+++ docker-compose.config.sh
@@ -4,5 +4,7 @@ set -eo pipefail
 #
 # Configuration file for docker compose properties, containers, variables etc.
 #
-readonly SERVER_CONTAINER_ID="ac-server"
-readonly CLUSTER_SERVICE_CONTAINER_ID="ac-cluster-service"
+export SERVER_CONTAINER_ID="ac-server"
+export CLUSTER_SERVICE_CONTAINER_ID="ac-cluster-service"
+export CLIENT_CONTAINER_ID="ac-client"
+export DB_CONTAINER_ID="ac-db"
--- proto/grpc.config.sh
+++ proto/grpc.config.sh
@@ -6,7 +6,7 @@ set -eo pipefail
 #
 # The following paths are used for copying generated gRPC files from docker containers to the actual project directory.
 # Project paths must be relative to build.sh.
-readonly BE_GRPC_PROJECT_PATH="./server/src/main/java/edu/setokk/astrocluster/grpc"
-readonly BE_GRPC_DOCKER_PATH="/home/app/src/main/java/edu/setokk/astrocluster/grpc"
-readonly CS_GRPC_PROJECT_PATH="./cluster/service"
-readonly CS_GRPC_DOCKER_PATH="/ac-clustering-service/service"
+export BE_GRPC_PROJECT_PATH="./server/src/main/java/edu/setokk/astrocluster/grpc"
+export BE_GRPC_DOCKER_PATH="/home/app/src/main/java/edu/setokk/astrocluster/grpc"
+export CS_GRPC_PROJECT_PATH="./cluster/service"
+export CS_GRPC_DOCKER_PATH="/ac-clustering-service/service"
